b03d02f8ce27d1ffd3a3a674e9ad4d5e082e8b70
Cho Hyunwoo <chodima0.e@gmail.com>
2025-04-18 23:17:07 +0900

    refactor: decompose app state

---
+++ src/app.rs
@@ -0,0 +1,129 @@
+use crate::canvas::{AppCanvas, AppCanvasProps};
+use crate::screenshot::Screenshot;
+use crate::window::create_window;
+
+use sdl2::{
+    event::Event, image::InitFlag, keyboard::Keycode as SDLKeycode, pixels::Color, rect::Rect, EventPump,
+};
+
+const MIN_ZOOM_SCALE: f32 = 1.0;
+const MAX_ZOOM_SCALE: f32 = 10.0;
+
+pub(crate) fn run_app() -> Result<(), String> {
+    let sdl_context = sdl2::init()?;
+    init_sdl2_image_context();
+
+    let canvas_props = AppCanvasProps::default();
+    let canvas = create_window(&canvas_props, &sdl_context);
+    let event_pump = sdl_context.event_pump().expect("Event pump failed to initialize");
+
+    run_event_loop(event_pump, canvas, canvas_props)?;
+
+    Ok(())
+}
+
+fn run_event_loop(mut event_pump: EventPump, mut canvas: AppCanvas, mut canvas_props: AppCanvasProps) -> Result<(), String> {
+    let screenshot = Screenshot::capture();
+    let mut texture = canvas.texture_creator.create_texture_streaming(
+        sdl2::pixels::PixelFormatEnum::RGBA32,
+        canvas_props.width,
+        canvas_props.height
+    ).expect("Failed to create the texture");
+
+    texture.update(None, screenshot.as_bytes(), (canvas_props.width * 4) as usize)
+        .expect("Texture update failed");
+
+    canvas.window_canvas.window_mut().show();
+
+    'running: loop {
+        let mouse_state = event_pump.mouse_state();
+        let mouse_x = mouse_state.x() as f32;
+        let mouse_y = mouse_state.y() as f32;
+
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit{..} | Event::KeyDown { keycode: Some(SDLKeycode::Escape), .. } => break 'running,
+                Event::KeyDown { keycode, .. } => {
+                    println!("{:?}", keycode);
+                },
+                Event::MouseWheel { y, .. } if y != 0 => {
+                    let prev_zoom = canvas_props.zoom_scale;
+
+                    canvas_props.zoom_scale = 
+                        (canvas_props.zoom_scale + y as f32 * 0.5).max(MIN_ZOOM_SCALE).min(MAX_ZOOM_SCALE);
+
+                    // Adjust offsets to zoom into the mouse cursor's position
+                    // Translate mouse position into the image coordinate space
+                    let mouse_x_img = (mouse_x - canvas_props.offset_x) / prev_zoom;
+                    let mouse_y_img = (mouse_y - canvas_props.offset_y) / prev_zoom;
+
+                    // Update offsets to ensure zoom focuses on cursor
+                    canvas_props.offset_x = mouse_x - mouse_x_img * canvas_props.zoom_scale;
+                    canvas_props.offset_y = mouse_y - mouse_y_img * canvas_props.zoom_scale;
+                },
+                Event::MouseMotion { xrel, yrel, mousestate, .. } => {
+                    if mousestate.left() {
+                        canvas_props.offset_x += xrel as f32;
+                        canvas_props.offset_y += yrel as f32;
+                    }
+                },
+
+                _ => ()
+            }
+        }
+
+        // Calculate destination (draw) size
+        let dst_width = canvas_props.width as f32 * canvas_props.zoom_scale;
+        let dst_height = canvas_props.height as f32 * canvas_props.zoom_scale;
+
+        // Border clamping
+        let min_offset_x = if dst_width > canvas_props.width as f32 {
+            canvas_props.width as f32 - dst_width
+        } else {
+            (canvas_props.width as f32 - dst_width) / 2.0
+        };
+        let max_offset_x = if dst_width > canvas_props.width as f32 {
+            0.0
+        } else {
+            (canvas_props.width as f32 - dst_width) / 2.0
+        };
+
+        let min_offset_y = if dst_height > canvas_props.height as f32 {
+            canvas_props.height as f32 - dst_height
+        } else {
+            (canvas_props.height as f32 - dst_height) / 2.0
+        };
+        let max_offset_y = if dst_height > canvas_props.height as f32 {
+            0.0
+        } else {
+            (canvas_props.height as f32 - dst_height) / 2.0
+        };
+
+        // Clamp the offsets so the image never leaves the window area
+        canvas_props.offset_x = crate::utils::clamp(canvas_props.offset_x, min_offset_x, max_offset_x);
+        canvas_props.offset_y = crate::utils::clamp(canvas_props.offset_y, min_offset_y, max_offset_y);
+
+        canvas.window_canvas.set_draw_color(Color::BLACK);
+        canvas.window_canvas.clear();
+
+        // Draw the texture
+        let dst_rect = Rect::new(
+          canvas_props.offset_x.round() as i32,
+          canvas_props.offset_y.round() as i32,
+            dst_width as u32,
+            dst_height as u32,
+        );
+
+        canvas.window_canvas.copy(&texture, None, Some(dst_rect))?;
+        canvas.window_canvas.present();
+    }
+
+    Ok(())
+}
+
+fn init_sdl2_image_context() {
+    if let Err(e) = sdl2::image::init(InitFlag::JPG) {
+        log::error!("SDL2 Image failed to load: {}", e);
+        panic!("SDL2 Image was not loaded");
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -2,135 +2,25 @@ mod system_tray;
 mod screenshot;
 mod utils;
 mod window;
-mod app_state;
+mod app;
 mod canvas;
 
 use std::sync::{Arc, Mutex};
-
-use device_query::{DeviceQuery, DeviceState, Keycode as DQKeycode};
-use screenshot::Screenshot;
+use app::run_app;
 use system_tray::run_tray_icon;
+use device_query::{DeviceQuery, DeviceState, Keycode as DQKeycode};
 
-use sdl2::{
-    pixels::Color,
-	event::Event,
-	keyboard::Keycode as SDLKeycode,
-	rect::Rect,
-};
-
-use crate::app_state::AppState;
-
-const MIN_ZOOM_SCALE: f32 = 1.0;
-const MAX_ZOOM_SCALE: f32 = 10.0;
-
-fn run_app() -> Result<(), String> {
-    let screenshot = Screenshot::capture();
-    let mut app_state = AppState::new()?;
-    let mut texture = app_state.canvas.texture_creator.create_texture_streaming(
-            sdl2::pixels::PixelFormatEnum::RGBA32,
-            app_state.canvas_props.width,
-            app_state.canvas_props.height
-        ).expect("Failed to create the texture");
-
-    texture.update(None, screenshot.as_bytes(), (app_state.canvas_props.width * 4) as usize)
-        .expect("Texture update failed");
-
-    let mut event_pump = app_state.sdl_context.event_pump()?;
-
-    app_state.canvas.window_canvas.window_mut().show();
-
-    'running: loop {
-        let mouse_state = event_pump.mouse_state();
-        let mouse_x = mouse_state.x() as f32;
-        let mouse_y = mouse_state.y() as f32;
-
-        for event in event_pump.poll_iter() {
-            match event {
-                Event::Quit{..} |
-                Event::KeyDown { keycode: Some(SDLKeycode::Escape), .. } => break 'running,
-
-                Event::KeyDown { keycode, .. } => {
-                    println!("{:?}", keycode);
-                },
-
-                Event::MouseWheel { y, .. } if y != 0 => {
-                    let prev_zoom = app_state.canvas_props.zoom_scale;
-
-                    app_state.canvas_props.zoom_scale = 
-                        (app_state.canvas_props.zoom_scale + y as f32 * 0.5).max(MIN_ZOOM_SCALE).min(MAX_ZOOM_SCALE);
-
-                    // Adjust offsets to zoom into the mouse cursor's position
-                    // Translate mouse position into the image coordinate space
-                    let mouse_x_img = (mouse_x - app_state.canvas_props.offset_x) / prev_zoom;
-                    let mouse_y_img = (mouse_y - app_state.canvas_props.offset_y) / prev_zoom;
-
-                    // Update offsets to ensure zoom focuses on cursor
-                    app_state.canvas_props.offset_x = mouse_x - mouse_x_img * app_state.canvas_props.zoom_scale;
-                    app_state.canvas_props.offset_y = mouse_y - mouse_y_img * app_state.canvas_props.zoom_scale;
-                },
-
-                // Mouse drag to pan
-                Event::MouseMotion { xrel, yrel, mousestate, .. } => {
-                    if mousestate.left() {
-                        app_state.canvas_props.offset_x += xrel as f32;
-                        app_state.canvas_props.offset_y += yrel as f32;
-                    }
-                },
-
-                _ => ()
-            }
-        }
-
-        // Calculate destination (draw) size
-        let dst_width = app_state.canvas_props.width as f32 * app_state.canvas_props.zoom_scale;
-        let dst_height = app_state.canvas_props.height as f32 * app_state.canvas_props.zoom_scale;
-
-        // Border clamping
-        let min_offset_x = if dst_width > app_state.canvas_props.width as f32 {
-            app_state.canvas_props.width as f32 - dst_width
-        } else {
-            (app_state.canvas_props.width as f32 - dst_width) / 2.0
-        };
-        let max_offset_x = if dst_width > app_state.canvas_props.width as f32 {
-            0.0
-        } else {
-            (app_state.canvas_props.width as f32 - dst_width) / 2.0
-        };
-
-        let min_offset_y = if dst_height > app_state.canvas_props.height as f32 {
-            app_state.canvas_props.height as f32 - dst_height
-        } else {
-            (app_state.canvas_props.height as f32 - dst_height) / 2.0
-        };
-        let max_offset_y = if dst_height > app_state.canvas_props.height as f32 {
-            0.0
-        } else {
-            (app_state.canvas_props.height as f32 - dst_height) / 2.0
-        };
-
-        // Clamp the offsets so the image never leaves the window area
-        app_state.canvas_props.offset_x = utils::clamp(app_state.canvas_props.offset_x, min_offset_x, max_offset_x);
-        app_state.canvas_props.offset_y = utils::clamp(app_state.canvas_props.offset_y, min_offset_y, max_offset_y);
-
-        app_state.canvas.window_canvas.set_draw_color(Color::BLACK);
-        app_state.canvas.window_canvas.clear();
-
-        // Draw the texture
-        let dst_rect = Rect::new(
-          app_state.canvas_props.offset_x.round() as i32,
-          app_state.canvas_props.offset_y.round() as i32,
-            dst_width as u32,
-            dst_height as u32,
-        );
-
-        app_state.canvas.window_canvas.copy(&texture, None, Some(dst_rect))?;
-        app_state.canvas.window_canvas.present();
+fn main() -> Result<(), String> {
+    { // Tray Application
+        run_tray_icon();
     }
+
+    run_keyboard_listener()?;
 
     Ok(())
 }
 
-fn run() -> Result<(), String> {
+fn run_keyboard_listener() -> Result<(), String> {
     let device_state = DeviceState::new();
 
     let is_app_running = Arc::new(Mutex::new(false));
@@ -153,14 +43,3 @@ fn run() -> Result<(), String> {
         }
     }
 }
-
-fn main() -> Result<(), String> {
-    { // Tray Application
-        run_tray_icon();
-    }
-
-    run()?;
-
-    Ok(())
-}
-
